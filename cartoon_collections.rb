4614da2dfa7fe0fa07a9346824effba5ef60b250
Samantha Hyatt <shhyatt@gmail.com>
2018-12-04 18:03:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  
+  # code an argument here
   # Your code here
 end
 
